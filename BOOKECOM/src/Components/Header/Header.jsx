8c69a1702f515ac5cfb3c4b43f6171827ef6114d
Asci Volkan <volknasc735@gmail.com>
2023-12-14 16:12:43 +0100

    responsve

--- BOOKECOM/src/Components/Header/Header.jsx
+++ BOOKECOM/src/Components/Header/Header.jsx
@@ -7,34 +7,43 @@ import 'swiper/css/navigation';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 
+import { GoArrowRight } from 'react-icons/go'
+import { GoArrowLeft } from 'react-icons/go'
+
+import headerShape from '../../assets/header-shape.svg'
 
 
 
-
-import { Link } from 'react-router-dom'
 
 
 function Header() {
     return (
         <header>
             <div className="header-container">
+
                 <Swiper
                     spaceBetween={50}
                     slidesPerView={1}
-                    modules={[Pagination, Navigation]}
-                    pagination={{ el: '.swiper-pagination', clickable: true }}
-                    navigation
                     loop={true}
+                    modules={[Navigation, Pagination]}
+                    pagination={{ el: '.swiper-pagination', clickable: true }}
+                    navigation={
+                        {
+                            preEl: '.button-prev-slide',
+                            nextEl: '.button-next-slide'
+                        }
+                    }
+
                 >
                     {headerBooks.map(({ tittle, info, img, btnLink }, index) => (
                         <SwiperSlide key={index}>
                             <div className="header-wrapper container">
                                 <div className="header-left">
                                     <h1>
-                                        {tittle}
-                                        <p dangerouslySetInnerHTML={{ __html: info }}></p>
-                                        <Link className='btn btn-border' to={btnLink} target='_blank'>Learn more</Link>
-                                    </h1>
+                                        {tittle}</h1>
+                                    <p dangerouslySetInnerHTML={{ __html: info }}></p>
+                                    <a href={btnLink} className='btn btn-border'>learn more</a>
+
                                 </div>
                                 <div className="header-right">
                                     <img src={img} alt="header" />
@@ -42,10 +51,17 @@ function Header() {
                             </div>
                         </SwiperSlide>
                     ))}
+                    <div className="slider-button">
+                        <div className="button-prev-slide slidebutton"><GoArrowLeft /></div>
+                        <div className="button-next-slide slidebutton"><GoArrowRight /></div>
+                    </div>
                     <div className="container">
                         <div className="swiper-pagination"></div>
                     </div>
                 </Swiper>
+                <div className="header-shape">
+                    <img src={headerShape} alt="" />
+                </div>
             </div>
         </header>
     );
